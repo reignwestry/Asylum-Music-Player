985e19ae8a836e66fd43eda9f7efb91f014c0eed
Reign <reignzj.westry@gmail.com>
2026-08-01 13:24:16 -0400

    Basic Structure set up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,88 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+function Header(){
+  return(
+    <View style={styles.header}>
+        <Text>Asylum</Text>
+    </View>
   );
 }
 
-export default function HomeScreen() {
+function Body(){
+  return(
+    <View style={styles.body}>
+
+    </View>
+  );
+}
+
+function BtmNav() {
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+    <View style={styles.btmNav}>
+      {/* <Text>btmNav</Text> */}
+      <Pressable
+        style={styles.btnContainer}
+        onPress={() => alert("btn1 clicked")}>
+        <Text>Library</Text>
+      </Pressable>
+      <Pressable
+        style={styles.btnContainer}
+        onPress={() => alert("btn2 clicked")}>
+        <Text>Playlists</Text>
+      </Pressable>
+      <Pressable
+        style={styles.btnContainer}
+        onPress={() => alert("btn3 clicked")}>
+        <Text>Player</Text>
+      </Pressable>
+      <Pressable
+        style={styles.btnContainer}
+        onPress={() => alert("btn4 clicked")}>
+        <Text>EQ</Text>
+      </Pressable>
+      <Pressable
+        style={styles.btnContainer}
+        onPress={() => alert("btn5 clicked")}>
+        <Text>Settings</Text>
+      </Pressable>
+    </View>
+  );
+}
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Body />
+      <BtmNav />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  body:{
+    height: "90%",
+    backgroundColor: "black",
+  },
+  header:{
+    backgroundColor: "lightgray",
+    width: "100%",
+    height: "5%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btmNav:{
+    height: "10%",
+    backgroundColor: "blue",
+    flexDirection: "row",
+  },
+  btnContainer: {
+    backgroundColor: "gray",
+    alignItems: "center",
+    width: "20%",
+    height: "100%",
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+    flexDirection: "column",
   },
 });
